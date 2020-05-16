99af5ab973899ff06f2ce81d99d296c62b02057b
looooopy <giorgio.salmi@stena.com>
2020-05-16 03:06:24 +0200

    - Initial version with SBUS to handle the Zumo 34U4

---
+++ README.md
@@ -0,0 +1,45 @@
+# Zumo 32U4 SBUS Receiver
+
+Using SBUS to communicate with the Zumo board. 
+You will also need a signal inverter between SBUS receiver and Zumo board [Build your own inverter](https://www.ernstc.dk/arduino/sbus.html).
+
+## Why use SBUS
+
+When using pwm we are restricted to use more input pins with SBUS we only need one.
+
+## Channels 
+
+| Stick    | Channel | Constant         | Description                                                     |
+| -------- | ------- | ---------------- | --------------------------------------------------------------- |
+| Throttle | 2       | THROTTLE_CHANNEL | Gas & break, Mode 2 Stick on the left side move UP or DOWN      |
+| Turning  | 0       | TURNING_CHANNEL  | Left & Right, Mode 2 stick on the right side move LEFT or RIGHT |
+| Reverse  | 8       | REVERSE_CHANNEL  | Make zumo go in reverse (Switch B on my Radiolink)              |
+
+## Status leds
+
+On the Zumo 32U4 board you got three leds yellow green red.
+
+| Led    | Mode      | Description                    |
+| ------ | --------- | ------------------------------ |
+| Red    | failSafe  | No connection to transmitter   |
+| Yellow | lostFrame | SBUS lost a frame              |
+| Green  | Ok        | We have connection to receiver |
+
+
+## Debug
+
+If you wich to se the exact mesurements from SBUS channels in terminal you have to hook it up to USB and connect with putty to it.
+This mode should not be use more than debug purpose, you will miss packages from SBUS and that is made intentially because i wish to delay the output so merasurements can be read from console.
+
+* First you need to uncomment in main.cpp
+
+    //#define MY_DEBUG 
+
+* Upload the scetch
+
+* Open putty with these settings
+   
+    Select Serial
+    Select your COM port
+    Select Speed: 115200
+    Window->Uncheck 'Push erased text to scrollback'
---
+++ src/main.cpp
@@ -0,0 +1,141 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "SBUS.h"
+#include <Zumo32U4.h>
+
+// Motor speed constants
+const int16_t MOTOR_MAX_FORWARD = 400;
+const int16_t MOTOR_OFF = 0;
+const int16_t MOTOR_MAX_REVERSE = -400;
+
+// Channel mapping
+const int8_t  THROTTLE_CHANNEL = 2;
+const int8_t  TURNING_CHANNEL = 0;
+const int8_t  REVERSE_CHANNEL = 8;
+
+// Max and min values for channels
+const int16_t CHANNEL_MIN = 330;
+const int16_t CHANNEL_MAX = 1600;
+const int16_t CHANNEL_MID_MIN = 980;
+const int16_t CHANNEL_MID_MAX = 1020;
+
+// Uncomment to output debug logging on usb
+// #define MY_DEBUG
+
+// Forward declaration
+#ifdef MY_DEBUG
+void debugSBUS (uint16_t* channels, bool failSafe, bool lostFrame);
+void terminalCls();
+#endif
+void mapTurningChannel(uint16_t turningChannel,  int16_t* speeds);
+void mapReverseChannel(uint16_t reverseChannel, int16_t* speeds);
+void mapThrottleChannel(uint16_t throttleChannel, int16_t* speeds);
+
+SBUS x8r(Serial1);
+Zumo32U4Motors motors;
+
+void setup() {
+  #ifdef MY_DEBUG
+  Serial.begin(115200);
+  #endif
+  // begin the SBUS communication
+	x8r.begin();
+
+  ledGreen(0);
+  ledRed(0);
+  ledYellow(0);
+  motors.setSpeeds(0,0);
+}
+
+void loop() {
+  uint16_t channels[16];
+  bool failSafe ;
+  bool lostFrame;
+
+  if(x8r.read(&channels[0], &failSafe, &lostFrame)){
+    #ifdef MY_DEBUG
+    debugSBUS(&channels[0], failSafe, lostFrame);
+    #endif
+    if(lostFrame) {
+      ledYellow(1);
+    } else {
+      ledYellow(0);
+    }
+    ledGreen(1);
+  }
+
+  if(failSafe) {
+    // Set motor speed to 0
+    motors.setSpeeds(0,0);
+    ledRed(1);
+  }
+  else
+  {
+    int16_t speeds[2];
+    mapThrottleChannel(channels[THROTTLE_CHANNEL], &speeds[0]);
+    mapReverseChannel(channels[REVERSE_CHANNEL], &speeds[0]);
+    mapTurningChannel(channels[TURNING_CHANNEL], &speeds[0]);
+
+    motors.setSpeeds(speeds[0],speeds[1]);
+    ledRed(0);
+  }
+}
+
+void mapThrottleChannel(uint16_t throttleChannel, int16_t* speeds) {
+  if(throttleChannel > CHANNEL_MIN && throttleChannel < CHANNEL_MAX ) {
+    speeds[0] = round((throttleChannel -CHANNEL_MIN) / 3.175);
+    speeds[1] = round((throttleChannel -CHANNEL_MIN) / 3.175);
+  } else if (throttleChannel < CHANNEL_MIN) {
+    speeds[0] = MOTOR_OFF;
+    speeds[1] = MOTOR_OFF;
+  } else if (throttleChannel > CHANNEL_MAX) {
+    speeds[0] = MOTOR_MAX_FORWARD;
+    speeds[1] = MOTOR_MAX_FORWARD;
+  }
+}
+
+void mapReverseChannel(uint16_t reverseChannel, int16_t* speeds) {
+  if( reverseChannel > CHANNEL_MAX ) {
+    speeds[0] = -speeds[0];
+    speeds[1] = -speeds[1];
+  }
+}
+
+void mapTurningChannel(uint16_t turningChannel,  int16_t* speeds) {
+  if(turningChannel > CHANNEL_MIN && turningChannel < CHANNEL_MAX ) {
+    if(turningChannel < CHANNEL_MID_MIN ) {
+      speeds[0] = round(speeds[0]  * (CHANNEL_MAX - turningChannel) / ( CHANNEL_MAX - CHANNEL_MID_MAX) );
+    } else if (turningChannel > CHANNEL_MID_MAX) {
+      speeds[1] = round(speeds[1]  * (turningChannel -CHANNEL_MIN) / (CHANNEL_MID_MIN - CHANNEL_MIN) );
+    }
+  } else if (turningChannel < CHANNEL_MIN) {
+    speeds[0] = MOTOR_MAX_REVERSE;
+    speeds[1] = MOTOR_MAX_FORWARD;
+  } else if (turningChannel > CHANNEL_MAX) {
+    speeds[0] = MOTOR_MAX_FORWARD;
+    speeds[1] = MOTOR_MAX_REVERSE;
+  }
+  
+}
+
+#ifdef MY_DEBUG
+void debugSBUS (uint16_t* channels, bool failSafe, bool lostFrame) {
+    Serial.println("----- Recieived ------");
+    failSafe && Serial.println("failSafe");
+    lostFrame && Serial.println("lostFrame");
+    Serial.println("----- Channels ------");
+    char buff[20];
+    for (int i = 0; i < 16; i++ ) {
+      sprintf(buff, "%d: %d", i, channels[i]);
+      Serial.println(buff);
+    }
+    Serial.println("--------------------");
+    Serial.println("");
+    delay(100);
+    terminalCls();
+}
+
+void terminalCls() {
+  Serial.write("\033\143");       // ESC command
+}
+#endif
